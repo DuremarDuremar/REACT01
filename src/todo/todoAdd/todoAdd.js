e7b8dab1d79b40214cdcc5e9546c28f6eb8b903a
DuremarDuremar <duremar98@yandex,ru>
2020-08-23 17:13:36 +0300

    todo

--- src/todo/todoAdd/todoAdd.js
+++ src/todo/todoAdd/todoAdd.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todoAdd.scss";
 
 const TodoAdd = ({ todoAdd }) => {
+  const [todoText, setTodoText] = useState("");
+
   const onSubmit = (event) => {
     event.preventDefault();
+    todoAdd(todoText);
+    setTodoText("");
+  };
+
+  const onLabelAdd = (e) => {
+    setTodoText(e.target.value);
   };
 
   return (
     <div className="todo__add">
       <form className="todo__add_form" onSubmit={onSubmit}>
-        <input></input>
-        <button onClick={() => todoAdd("Run")}>
+        <input type="text" onChange={onLabelAdd} value={todoText}></input>
+        <button>
           <i className="far fa-bell fa-2x"></i>
         </button>
       </form>
