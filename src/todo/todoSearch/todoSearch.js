5ee43fd35fc40ebb389e378aa942b0a7d5e0fef1
DuremarDuremar <duremar98@yandex,ru>
2020-08-24 15:25:38 +0300

    todo.change

--- src/todo/todoSearch/todoSearch.js
+++ src/todo/todoSearch/todoSearch.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todoSearch.scss";
 
-const TodoSearch = ({ value, setValue, setTodoFilter }) => {
+const TodoSearch = ({ value, setValue, todoFilter, setTodoFilter }) => {
+  const [classBtn, setClassBtn] = useState([
+    { id: "all", className: "todo__btn_active" },
+    { id: "active", className: "" },
+    { id: "done", className: "" },
+  ]);
+
   const onLabelSearch = (e) => {
     setValue(e.target.value);
   };
 
   const onButton = (text) => {
     setTodoFilter(text);
+    setClassBtn(
+      classBtn.map(function (item) {
+        if (item.id === text) {
+          item.className = "todo__btn_active";
+        } else {
+          item.className = "";
+        }
+        return item;
+      })
+    );
   };
 
   return (
     <div className="todo__search">
       <div className="todo__search_form">
-        <button className="todo__search_all" onClick={() => onButton("all")}>
+        <button
+          className={classBtn[0].className}
+          id={classBtn[0].id}
+          onClick={() => onButton("all")}
+        >
           <i className="fab fa-buffer fa-2x"></i>
         </button>
         <div className="todo__search_input">
@@ -21,13 +41,15 @@ const TodoSearch = ({ value, setValue, setTodoFilter }) => {
         </div>
         <div>
           <button
-            className="todo__search_active"
+            className={classBtn[1].className}
+            id={classBtn[1].id}
             onClick={() => onButton("active")}
           >
             <i className="far fa-bookmark fa-2x"></i>
           </button>
           <button
-            className="todo__search_done"
+            className={classBtn[2].className}
+            id={classBtn[2].id}
             onClick={() => onButton("done")}
           >
             <i className="fas fa-bookmark fa-2x"></i>
