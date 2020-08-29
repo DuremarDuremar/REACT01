2968fcdbd8e4b6d37708fa47da9f43e981c9c952
DuremarDuremar <duremar98@yandex,ru>
2020-08-29 19:05:44 +0300

    cinema

--- src/cinema/cinema.js
+++ src/cinema/cinema.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CinemaHeader from "./cinemaHeader/cinemaHeader";
 import CinemaMain from "./cinemaMain/cinemaMain";
 import "./cinema.scss";
 
 const Cinema = () => {
+  const [cinemaFest, setCinemaFest] = useState("Cannes");
+
+  const cinemaFestActive = (item) => {
+    setCinemaFest(item);
+    return item;
+  };
+
+  console.log(cinemaFest);
+
   return (
     <div className="cinema">
-      <CinemaHeader />
+      <CinemaHeader
+        cinemaFestActive={cinemaFestActive}
+        cinemaFest={cinemaFest}
+      />
       <CinemaMain />
       <footer className="cinema__footer"></footer>
     </div>
--- src/cinema/cinemaHeader/cinemaHeader.js
+++ src/cinema/cinemaHeader/cinemaHeader.js
@@ -2,15 +2,55 @@ import React from "react";
 import "./cinemaHeader.scss";
 import logo from "../cinemaImages/cinemaLogo.jpg";
 
-const CinemaHeader = () => {
+const CinemaHeader = ({ cinemaFestActive, cinemaFest }) => {
+  // let classActiveFest = "cinema__active_fest";
+  // // item.id === Number(cinemaFest) ? "cinema__active_fest" : null;
+
+  const btnFest = [
+    {
+      id: "Sundance",
+      label: "Sundance",
+      classNames: "",
+    },
+    {
+      id: "Venice",
+      label: "Venice",
+      classNames: "",
+    },
+    {
+      id: "Cannes",
+      label: "Cannes",
+      classNames: "",
+    },
+    {
+      id: "Berlin",
+      label: "Berlin",
+      classNames: "",
+    },
+  ];
+
+  const festBtn = btnFest.map(function (item) {
+    if (item.id == cinemaFest) {
+      item.classNames += "cinema__active_fest";
+    }
+    return (
+      <h2 className={item.classNames} id={item.id}>
+        {item.label}
+      </h2>
+    );
+  });
+
   return (
     <header className="cinema__header">
       <div className="cinema__container cinema__header_wrapper">
-        <div className="cinema__titls">
-          <h2>Sundance</h2>
-          <h2>Venice</h2>
-          <h2>Cannes</h2>
-          <h2>Berlin</h2>
+        <div
+          className="cinema__titls"
+          onClick={(event) => cinemaFestActive(event.target.id)}
+        >
+          {festBtn[0]}
+          {festBtn[1]}
+          {festBtn[2]}
+          {festBtn[3]}
         </div>
         <div className="cinema__logo">
           <img src={logo} alt="logo"></img>
