ef919d39dc8acec2ed3b69965b37f53ebf0d4e27
DuremarDuremar <duremar98@yandex,ru>
2020-09-03 23:26:53 +0300

    Updates

---
+++ precache-manifest.98c36c340f89f040c7253e37cb9a5d74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f7da805de007a6a294341ac412cd53",
+    "url": "/REACT01.git/index.html"
+  },
+  {
+    "revision": "62dea6f7150dbcc1b1c7",
+    "url": "/REACT01.git/static/css/main.0929c788.chunk.css"
+  },
+  {
+    "revision": "e780ef0fb00577bac870",
+    "url": "/REACT01.git/static/js/2.7425dff9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACT01.git/static/js/2.7425dff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dea6f7150dbcc1b1c7",
+    "url": "/REACT01.git/static/js/main.5d06a990.chunk.js"
+  },
+  {
+    "revision": "90b6d1dd29c9d53771b4",
+    "url": "/REACT01.git/static/js/runtime-main.8ae68432.js"
+  },
+  {
+    "revision": "748b8ccd35b609e5259282d5e302117d",
+    "url": "/REACT01.git/static/media/Ivan.748b8ccd.jpg"
+  },
+  {
+    "revision": "9b75234f891453b181e2ab15e25dd6a0",
+    "url": "/REACT01.git/static/media/cannes.9b75234f.png"
+  },
+  {
+    "revision": "0d64a45e2f26c7d3dfd3fece962474f7",
+    "url": "/REACT01.git/static/media/cinemaLogo.0d64a45e.jpg"
+  },
+  {
+    "revision": "63638e8a32012f294a4bc8853840d892",
+    "url": "/REACT01.git/static/media/danse-macabre.63638e8a.png"
+  }
+]);
